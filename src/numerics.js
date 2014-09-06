3058e55762a46d144ee0c01669c9b05975cdb3ae
Jeff Plourde <jeff@mdpnp.org>
2014-09-06 12:20:58 -0400

    Monitors even more data topics

--- src/numerics.js
+++ src/numerics.js
@@ -4,17 +4,17 @@ var PartitionBox = require('./partition-box.js');
 
 var DOMAINID = 15;
 
-function TableManager(openICE, tableName, htmlTable, keyFields, valueHandler) {
+function TableManager(openICE, tableName, htmlTable, keyFields, valueHandler, valueColumns) {
   this.openICE = openICE;
   this.table = null;
   this.keyFields = keyFields;
   this.tableName = tableName;
   this.htmlTable = htmlTable;
   this.valueHandler = valueHandler;
+  this.valueColumns = valueColumns;
 }
 
 TableManager.prototype.changePartition = function(partition) {
-  console.log(this.tableName + " changing to " + partition);
   if(this.table != null) {
     this.openICE.destroyTable(this.table);
     this.table = null;
@@ -23,7 +23,6 @@ TableManager.prototype.changePartition = function(partition) {
   var self = this;
 
   this.table.on('sample', function(e) {
-    console.log("I SEE A SAMPLE", self.tableName);
     var openICE = e.openICE, table = e.table, row = e.row, sample = e.sample;
     var tr = document.getElementById(self.tableName+row.rowId);
     if(typeof tr === 'undefined' || tr == null) {
@@ -34,16 +33,16 @@ TableManager.prototype.changePartition = function(partition) {
         td.innerHTML = row.keyValues[self.keyFields[i]];
         tr.appendChild(td);  
       }
-      
-      td = document.createElement("td");
-      td.id = self.tableName+row.rowId+"value";
-      td.innerHTML = "";
-      tr.appendChild(td);
+      tr.valueTds = [];
+      for(var i = 0; i < self.valueColumns; i++) {
+        var td = document.createElement("td");
+        tr.valueTds.push(td);
+        tr.appendChild(td);
+      }
 
       self.htmlTable.appendChild(tr);
     }
-    var td = document.getElementById(self.tableName+row.rowId+"value");
-    td.innerHTML = self.valueHandler(sample.data);
+    self.valueHandler(tr.valueTds, sample.data);
   });
   this.table.on('afterremove', function(e) {
     var openICE = e.openICE, table = e.table, row = e.row;
@@ -57,22 +56,71 @@ TableManager.prototype.changePartition = function(partition) {
 window.onload = function() {
   var select = document.getElementById('partitionBox');
   var openICE = new OpenICE("http://www.openice.info");
+  var tables = [];
 
   PartitionBox(openICE, select, DOMAINID);
 
-  var numeric = new TableManager(openICE, "Numeric", document.getElementById("Numeric"), ["unique_device_identifier", "metric_id", "instance_id"], function(data) { return data.value; });
-  var patientAlert = new TableManager(openICE, "PatientAlert", document.getElementById("PatientAlert"), ["unique_device_identifier", "identifier"], function(data) { return data.text; });
-  var technicalAlert = new TableManager(openICE, "TechnicalAlert", document.getElementById("TechnicalAlert"), ["unique_device_identifier", "identifier"], function(data) { return data.text; });
-  var deviceAlertCondition = new TableManager(openICE, "DeviceConditionAlert", document.getElementById("DeviceAlertCondition"), ["unique_device_identifier"], function(data) { return data.alert_state; });
-  var patientAssessment = new TableManager(openICE, "PatientAssessment", document.getElementById("PatientAssessment"), [], function(data) { return JSON.stringify(data); });
+  openICE.on('open', function(e) {
+    document.getElementById('status').innerHTML = "Connected";
+  });
+  openICE.on('close', function(e) {
+    document.getElementById('status').innerHTML = "Disconnected";
+  });
 
+  tables.push(new TableManager(openICE, "DeviceIdentity", document.getElementById("DeviceIdentity"),
+        ["unique_device_identifier"],
+        function(tds, data) { tds[0].innerHTML = data.manufacturer; tds[1].innerHTML = data.model; tds[2].innerHTML = data.serial_number;
+                              while (tds[3].firstChild) {
+                                 tds[3].removeChild(tds[3].firstChild);
+                              }
+                              var img = new Image();
+                              img.src = "data:image/png;base64, " + data.icon.raster;
+                              tds[3].appendChild(img);
+                              tds[4].innerHTML = data.build;},
+        5));
+  tables.push(new TableManager(openICE, "DeviceConnectivity", document.getElementById("DeviceConnectivity"),
+        ["unique_device_identifier"],
+        function(tds, data) { tds[0].innerHTML = data.state; tds[1].innerHTML = data.type; tds[2].innerHTML = data.info; tds[3].innerHTML = data.valid_targets; },
+        4));
+  tables.push(new TableManager(openICE, "AlarmSettings", document.getElementById("AlarmSettings"),
+        ["unique_device_identifier", "metric_id"],
+        function(tds, data) { tds[0].innerHTML = data.lower; tds[1].innerHTML = data.upper; },
+        2));  
+  tables.push(new TableManager(openICE, "GlobalAlarmSettingsObjective", document.getElementById("GlobalAlarmSettingsObjective"),
+        ["metric_id"],
+        function(tds, data) { tds[0].innerHTML = data.lower; tds[1].innerHTML = data.upper; },
+        2));    
+  tables.push(new TableManager(openICE, "LocalAlarmSettingsObjective", document.getElementById("LocalAlarmSettingsObjective"),
+        ["unique_device_identifier", "metric_id"],
+        function(tds, data) { tds[0].innerHTML = data.lower; tds[1].innerHTML = data.upper; },
+        2));      
+  tables.push(new TableManager(openICE, "Numeric", document.getElementById("Numeric"), 
+        ["unique_device_identifier", "metric_id", "instance_id"], 
+        function(tds, data) { tds[0].innerHTML = data.value; },
+        1));
+  tables.push(new TableManager(openICE, "PatientAlert", document.getElementById("PatientAlert"), 
+        ["unique_device_identifier", "identifier"], 
+        function(tds, data) { tds[0].innerHTML = data.text; },
+        1));
+  tables.push(new TableManager(openICE, "TechnicalAlert", document.getElementById("TechnicalAlert"), 
+        ["unique_device_identifier", "identifier"], 
+        function(tds, data) { tds[0].innerHTML = data.text; },
+        1));
+  tables.push(new TableManager(openICE, "DeviceConditionAlert", document.getElementById("DeviceAlertCondition"), 
+        ["unique_device_identifier"], 
+        function(tds, data) { tds[0].innerHTML = data.alert_state; },
+        1));
+  tables.push(new TableManager(openICE, "PatientAssessment", document.getElementById("PatientAssessment"), 
+        [], function(tds, data) { tds[0].innerHTML = JSON.stringify(data); },
+        1));
+  tables.push(new TableManager(openICE, "InfusionStatus", document.getElementById("InfusionStatus"), 
+        [], function(tds, data) { tds[0].innerHTML = JSON.stringify(data); },
+        1));
 
   var changePartition = function(partition) {
-    numeric.changePartition(partition);
-    patientAlert.changePartition(partition);
-    technicalAlert.changePartition(partition);
-    deviceAlertCondition.changePartition(partition);
-    patientAssessment.changePartition(partition);
+    for(var i = 0; i < tables.length; i++) {
+      tables[i].changePartition(partition);
+    }
   };
 
   select.onchange = function(e) {
