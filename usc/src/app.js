9000a0107d684dcd0f3842bf6010d28b3cc4cede
Jeff Plourde <jeff@mdpnp.org>
2014-09-10 17:59:31 -0400

    some functional angular.js mojo achieved

--- usc/src/app.js
+++ usc/src/app.js
@@ -4,6 +4,37 @@ var OpenICE = require('../../src/openice.js');
 (function() {
   var app = angular.module('usc', []);
 
+  app.factory('openice', function ($rootScope) {
+    var openICE = new OpenICE("http://dev.openice.info:80");
+    console.log(openICE);
+    return {
+      on: function (eventName, callback) {
+        openICE.on(eventName, function () {  
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(openICE, args);
+          });
+        });
+      },
+      createTableAndRegister: function (a, eventName, callback) {
+
+        var table = openICE.createTable(a);
+        for(var i = 0; i < eventName.length; i++) {
+          table.on(eventName[i], function() {
+            var args = arguments;
+            $rootScope.$apply(function () {
+              if (callback) {
+                callback.apply(openICE, args);
+              }
+            });
+          });
+        }
+        return table;
+      }
+    };
+  });
+
+
   app.directive("navbar", function() {
     return {
       restrict: 'E',
@@ -11,18 +42,20 @@ var OpenICE = require('../../src/openice.js');
     };
   });
 
-  app.controller("DeviceIdentityController", function() {
+  app.controller("DeviceIdentityController", ['$scope', 'openice', function($scope, openice) {
     var devices = this;
+    devices.deviceNames = {};
+    var partition = ["ICU"];
 
-    devices.openICE = new OpenICE("http://dev.openice.info:80");
 
-    devices.table = devices.openICE.createTable({domain:15,topic:'DeviceIdentity',partition:[]});
+    devices.deviceTable = openice.createTableAndRegister({domain:15,'partition':partition,topic:'DeviceIdentity'}, ['sample','afterremove'], function(foo) {
+    });
+    // Maintain a hash of device make/model by UDI
+    devices.deviceTable.on('sample', function(e) {
+      devices.deviceNames[e.row.keyValues.unique_device_identifier] = e.sample.data.manufacturer + " " + e.sample.data.model;
+    });
 
-    devices.deviceList = ['foo','bar','yoooo', new Date()];
-    // devices.table.on('sample', function(e) {
-    //   console.log(e.sample.data);
-    //   devices.deviceList.push(e.sample.data);
-      // diTable = JSON.stringify(e.sample.data);
-    // });
-  });
+    devices.numericTable = openice.createTableAndRegister({domain:15,'partition':partition,topic:'Numeric'}, ['sample','afterremove'], function(foo) {
+    });
+  }]);
 })();
