50d9e911ca09a4d28c5da917ac4194227df7fa2d
GlassFish User <gfish@arvi.mgh.harvard.edu>
2015-05-12 10:43:54 -0400

    derive patient partition names from FHIR

--- src/partition-box.js
+++ src/partition-box.js
@@ -1,45 +1,65 @@
-function fetchPatients() {
-	var url = "https://fhir.openice.info/fhir/Patient";
+var patientNames = {};
 
-	var client = new XMLHttpRequest();
+// Previous selection at top level scope because the previously
+// selected item may transiently "disappear" from the known list
+// of available partitions but for a consistent user experience
+// when it returns it ought to still be selected
+var previouslySelected = {};
 
-	client.onload = function(e) {
-		if (client.readyState === 4) {
-    		if (client.status === 200) {
-      			console.log(client.responseText);
-    		} else {
-      			console.error(client.statusText);
-    		}
-    	}
-	};
+function populatePatientName(partition, option) {
+   if(patientNames[partition]) {
+     option.text = patientNames[partition];
+   } else {
+        // This placeholder will prevent re-querying while awaiting a response
+        patientNames[partition] = partition;
+        var url = "https://fhir.openice.info/fhir/Patient?identifier="+partition.replace("MRN=","");
+        var client = new XMLHttpRequest();
 
-	client.onerror = function(e) {
-		console.error(client.statusText);
-	}
+        client.onload = function(e) {
+            if (client.readyState === 4) {
+                if (client.status === 200) {
+	           var bundle = JSON.parse(client.responseText);
+	           if(bundle.entry && bundle.entry.length > 0) {
+			var patient = bundle.entry[0].resource;
+			if(patient && patient.name && patient.name.length > 0) {
+		            var name = patient.name[0].given + " " + patient.name[0].family;
+		            patientNames[partition] = name;
+		            option.text = patientNames[partition];
+			    return;
+			}
+		    }				
+                } else {
+                        console.error(client.statusText);
+                }
+        }
+	    // If we didn't actually get a name then query again in the future
+            delete patientNames[partition];
+        };
 
-	client.open("GET", url, true);
+        client.onerror = function(e) {
+                console.error(client.statusText);
+	     delete patientNames[partition]; 
+        }
 
-	// client.setRequestHeader("Content-Type", "application");
+        client.open("GET", url, true);
 
-	client.send();
+        // client.setRequestHeader("Content-Type", "application");
+
+        client.send();
+    }
 
 }
 
 function rebuildSelect(e, select) {
-	//fetchPatients();
-
 	var table = e.table, row = e.row;
 	var names = {};
-	var previouslySelected = {};
 
 	for(var i = 0; i < select.options.length; i++) {
-		if(select.options[i].selected) {
-			previouslySelected[select.options[i].value] = 'selected';
-		}
+		previouslySelected[select.options[i].value] = select.options[i].selected;
 	}
 	select.options.length = 0;
-
-	names["<default>"] = "";
+        // No longer allow the default partition
+	// names["<default>"] = "";
 
 	var rowKeys = Object.keys(table.rows);
 	for(var i = 0; i < rowKeys.length; i++) {
@@ -48,11 +68,13 @@ function rebuildSelect(e, select) {
 			var name = row.samples[row.samples.length-1].data.partition.name;
 
 			if(typeof name === 'undefined' || null == name || name.length==0) {
-				names["<default>"] = "";
+                                // No longer allow default partition
+				// names["<default>"] = "";
 			} else {
 				for(var j = 0; j < name.length; j++) {
 					if(name[j]=="") {
-						names["<default>"] = "";
+						// No longer allow default partition
+						// names["<default>"] = "";
 					} else {
 						// Only include MRN patient partitions
 						if(0 != name[j].indexOf("MRN=")) { continue; }
@@ -63,13 +85,13 @@ function rebuildSelect(e, select) {
 		}
 	}
 	var partKeys = Object.keys(names);
-
 	for(var i = 0; i < partKeys.length; i++) {
 		var opt = document.createElement("option");
+                select.add(opt);
 		opt.text = partKeys[i];
 		opt.value = names[partKeys[i]];
 		opt.selected = previouslySelected[opt.value];
-		select.add(opt);
+                populatePatientName(partKeys[i], opt);
 	}		
 };
 
@@ -93,10 +115,11 @@ function PartitionBox(openICE, selectElement, domainId, changePartition, default
 	}
 
 	var opt = document.createElement("option");
-	opt.text = "<default>";
+        selectElement.add(opt);
+	opt.text = defaultPartition;
 	opt.value = defaultPartition;
-	opt.selected = 'selected';
-	selectElement.add(opt);
+	opt.selected = true;
+        populatePatientName(defaultPartition, opt);
 
 	selectElement.onchange = function(e) {
     	var partitions = [];
